bceb9b2f1eeecd4e570a4053f6e1058882ef2072
FadhilZein <fadhil.kid30@gmail.com>
2022-04-05 16:51:04 +0700

    Innit

---
+++ application/views/v_about.php
@@ -0,0 +1,25 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Fadhil</li>
+        <li>Nama Belakang : Zein</li>
+    </ul>
+    <br>
+    
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> Jl.in aja dulu jadian nya nanti</li>
+    </ul>
+    
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Konoha</li>
+    </ul>
+    
+    <h4>Olah Raga Favorit</h4>
+    <ul type="square">
+        <li>Memanah</li>
+        <li>Berkuda</li>
+    </ul>
+</section>
---
+++ application/views/v_index.php
@@ -0,0 +1,14 @@
+<section>
+        <h1><?php echo $judul ?></h1>
+        <p align='justify'>Aku suka kamu</p>
+
+        <ol type="a">
+        <li>Model</li>
+        <p align='justify'>Tapi kamu nya gak suka sama aku</p>
+        <p align='justify'>Aku nya jadi sedih</p>
+        <li>View</li>
+        <p align='justify'>Tapi gapapa, harus tetep senyum</p>
+        <li>Controller</li>
+        <p align='justify'>Tapi gabisa senyum, yaudah deh nangis aja</p>
+        <p align='justfy'>hiks hiks hiks hiks</p>
+</section>
